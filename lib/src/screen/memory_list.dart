d8ace83e9d6f5266d51874a2b93bd5a9950e6e16
seunghoonLee <zinozico0070@gmail.com>
2021-02-25 16:47:38 +0900

    sdfsdfsdf

--- lib/src/screen/memory_list.dart
+++ lib/src/screen/memory_list.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todays_memory/src/contorller/app_controller.dart';
+import 'package:todays_memory/src/screen/add_memory.dart';
 
 class MemoryListPage extends GetView<AppController> {
+  AddMemoryPage addMemoryPage = AddMemoryPage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +32,11 @@ class MemoryListPage extends GetView<AppController> {
                   return Container(
                     height: 100,
                     child: ListTile(
+                      //TODO : 오늘의 기억 한줄요약.
                       title: Text(controller.category[index]),
-                      subtitle: Text(index.toString()),
+                      subtitle: Text(
+                        addMemoryPage.dateFormat.format(DateTime.now()),
+                      ),
                       leading: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -40,6 +46,7 @@ class MemoryListPage extends GetView<AppController> {
                           ),
                         ],
                       ),
+                      //TODO : Delete 자리.
                       trailing: IconButton(
                         icon: Icon(Icons.close),
                         onPressed: () {},
